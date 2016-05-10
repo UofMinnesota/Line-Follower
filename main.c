45a0eacae6136cea56f584dd32a85e51e1d68422
Miguel Pfitscher <pfits002@umn.edu>
2016-05-10 16:59:14 -0500

    interrupt to verifying into Timer0

--- main.c
+++ main.c
@@ -11,14 +11,16 @@
 #ifndef F_CPU
 #define F_CPU 16000000ul
 #endif
-#define MinSpeed -30
-#define MaxSpeed 30
-uint16_t count_m1 = 30;
-uint16_t count_m2 = 30;
+#define MinSpeed -20
+#define MaxSpeed 20
+uint16_t count_m1 = 20;
+uint16_t count_m2 = 20;
+int16_t diffVal = 0;
 
 inline void setupLeds(){
 	DDRC |= (1 << DDC7);	// yellow led
 	DDRD |= (1 << DDD5);	// green led
+	DDRB |= (1 << DDB0);	// red led
 }
 
 
@@ -30,45 +32,58 @@ inline void toggle_greenled(){
 	PORTD ^= (1 << PORTD5);
 }
 
-
+void Timer0setup(){//1ms - 1000hz
+	TCCR0A |= (1 << WGM01);    // Enables CTC mode for timer0
+	TCCR0B |= (1 << CS01) | (1 << CS00); // For setting the timer offset value to 64
+	OCR0A = 249; // Sets the counter to 249
+	TIMSK0 |= (1 << OCIE0A); // Enable time keeper (timer0)
+}
 
 int main(void){
 	USBCON = 0;
 	setupLeds();
 	sensorDataSetup();
 	Timer1setup();
+	Timer0setup();
 	setPwm_Timer1_M2(count_m2);
   setPwm_Timer1_M1(count_m1);
+	_delay_ms(100); //delay to be sure that it wont get garbage from sensor.
 	sei();
-	int diffVal = 0;
 
 	while(1){
-
-		if(turn_right){
-			/*if(count_m1 > MinSpeed)
-				count_m1--;
-			if(count_m2 < MaxSpeed)
-				count_m2++;*/
-				if(diffVal < MaxSpeed)
-					diffVal ++;
-
-
-		}else if(turn_left){/*
-			if(count_m2 > MinSpeed)
-				count_m2--;
-			if(count_m1 < MaxSpeed)
-				count_m1++;*/
-				if(diffVal > MinSpeed)
-					diffVal --;
+		if(diffVal != 0){
+			PORTB &= ~(1 << PORTB0);
+		}else{
+			PORTB |= (1 << PORTB0);
 		}
 		setPwm_Timer1_M2(count_m2 - diffVal);
 	  setPwm_Timer1_M1(count_m1 + diffVal);
-		_delay_ms(1);
 
 	}
 }
 
+ISR(TIMER0_COMPA_vect){
+	if(!(PINB & (1 << PINB1))){ /* it is crossing the line */
+		/*turn right*/
+		/*data 1*/
+		if(diffVal > MinSpeed)
+			diffVal--;
 
+		PORTC |= (1 << PORTC7);
+	}
+	else if(!(PINB & (1 << PINB2))){ /* if there is line */
+		/*turn left*/
+		/*data 4*/
+		if(diffVal < MaxSpeed)
+			diffVal++;
+		PORTD &= ~(1 << PORTD5);
+	}
+	else{ /* in line */
+		diffVal = 0;
+		PORTC &= ~(1 << PORTC7);
+		PORTD |= (1 << PORTD5);
+	}
+}
 
 
 ISR(__vector_default){}
--- sensor.c
+++ sensor.c
@@ -11,24 +11,7 @@ void sensorDataSetup(){
 	DDRC |= (1 << DDC6);
 	PORTC |= (1 << PORTC6);
 	/*data interrupt*/
-	PCICR |= (1 << PCIE0);
-	PCMSK0 |= (1 << PCINT1); /*enable pcint1 and pcint2 isr*/
-	PCMSK0 |= (1 << PCINT2);
-}
-
-ISR(PCINT0_vect){
-
-	if(PINB & (1 << PINB1)){ //if there is line
-		/*turn right*/
-		/*data 1*/
-		turn_right = 1;
-		turn_left = 0;
-		PORTC ^= (1 << PORTC7);
-	}else if(PINB & (1 << PINB2)){ //if there is line
-		/*turn left*/
-		/*data 4*/
-		turn_left = 1;
-		turn_right = 0;
-		PORTD ^= (1 << PORTD5);
-	}
+	//PCICR |= (1 << PCIE0);
+	//PCMSK0 |= (1 << PCINT1); /*enable pcint1 and pcint2 isr*/
+	//PCMSK0 |= (1 << PCINT2);
 }
